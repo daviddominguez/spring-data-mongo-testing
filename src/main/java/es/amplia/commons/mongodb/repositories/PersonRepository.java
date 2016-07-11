95d2131162b753893fd6be503842b79adcbaface
David Dominguez <david.dominguez@amplia.es>
2016-07-11 13:22:37 +0200

    feat(spring-data-mongo-testing): add query methods to repository
    add several methods for query person objects into person repository
    interface using spring data query builder for repository interfaces

--- src/main/java/es/amplia/commons/mongodb/repositories/PersonRepository.java
+++ src/main/java/es/amplia/commons/mongodb/repositories/PersonRepository.java
@@ -2,7 +2,18 @@ package es.amplia.commons.mongodb.repositories;
 
 import es.amplia.commons.mongodb.model.Person;
 import org.bson.types.ObjectId;
+import org.joda.time.LocalDate;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.List;
+
 public interface PersonRepository extends MongoRepository<Person, ObjectId> {
+
+    Person findById(ObjectId id);
+    List<Person> findBySalaryBetween(BigDecimal from, BigDecimal to);
+    List<Person> findByBirthDateAfter(LocalDate date);
+    List<Person> findByAddressCountryIdIn(Collection<ObjectId> countries);
+    List<Person> findByAddressTagsIn(Collection<String> tags);
 }
