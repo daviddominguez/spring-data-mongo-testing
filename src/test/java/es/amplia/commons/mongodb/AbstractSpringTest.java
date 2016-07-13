94bb8e7243dd6c311a15911e9288f2d2699102ad
David Dominguez <david.dominguez@amplia.es>
2016-07-13 10:44:35 +0200

    refactor(spring-data-mongo-testing): use test configuration
    when running spring tests

--- src/test/java/es/amplia/commons/mongodb/AbstractSpringTest.java
+++ src/test/java/es/amplia/commons/mongodb/AbstractSpringTest.java
@@ -1,11 +1,10 @@
 package es.amplia.commons.mongodb;
 
-import es.amplia.commons.mongodb.configuration.MongoConfiguration;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {MongoConfiguration.class})
+@ContextConfiguration(classes = {TestMongoConfiguration.class})
 public class AbstractSpringTest {
 }
